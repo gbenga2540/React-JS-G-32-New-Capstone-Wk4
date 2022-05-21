c92015c159508214bd8d9dc2d097739dd0bfb3de
GBENGA254 <akindejuhmikel@gmail.com>
2022-05-21 14:34:12 +0100

    product page fix

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,12 +1,107 @@
-import React from 'react';
-
+import { useState, useEffect, React } from 'react';
+import './products.scss';
 const Products = ({ setPageNumber }) => {
+
   setPageNumber(2);
+  const [value, setValue] = useState(0);
+  const [value2, setValue2] = useState(0);
+  const [topProducts, setTopProducts] = useState();
+  const [allProducts, setAllProducts] = useState();
+  const username = sessionStorage.getItem('admin-username');
+
+  useEffect(() => {
+    try {
+      fetch('https://fakerapi.it/api/v1/products?_quantity=3')
+        .then(response => response.json())
+        .then(data => setTopProducts(data.data))
+    } catch (error) {
+      console.error(error)
+    }
+  }, [])
+
+  useEffect(() => {
+    try {
+      fetch('https://fakerapi.it/api/v1/products?_quantity=9')
+        .then(response => response.json())
+        .then(data => setAllProducts(data.data))
+    } catch (error) {
+      console.error(error)
+    }
+  }, [])
+
+  const handleClick = () => {
+    let form = document.getElementById('form');
+    var progress = document.getElementById('progress');
+    form.style.display = 'none';
+
+    // Animation for progress bar
+    setTimeout(() => {
+      progress.style.display = 'block';
+    }, 1000);
+
+    setTimeout(() => {
+      setInterval(() => { setValue(prev => prev !== 95 ? prev + 5 : prev) }, 125);
+      setInterval(() => { setValue2(prev => prev !== 75 ? prev + 5 : prev) }, 125);
+    }, 1500);
+  }
+
   return (
-    <div>
-        Products
+    <div className='products'>
+      <h1 id='welcome'>Welcome {username}</h1>
+      <h2>Let's take a look at the top selling products</h2>
+      <hr id="short" />
+
+      <div className="grid">
+        {topProducts && topProducts.map((item) => (
+          <div className="card">
+            <img src={item.image} alt="#" />
+            <div className="container">
+              <p>Name: {item.name}</p>
+              <p>Net_price: {item.net_price}</p>
+              <p>Tax: {item.taxes}</p>
+              <p>Price: {item.price}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <br />
+      <br />
+
+      <form id="form">
+        <h3>What do you think we should do?</h3>
+        <input type='radio' onClick={handleClick} /><p style={{ color: 'black' }}>Continue selling these Products.</p><br />
+        <input type='radio' onClick={handleClick} /><p style={{ color: 'red' }}>Lock don't sell for a while.</p>
+      </form>
+
+      <div id='progress'>
+        <h3>Weekly Poll Results</h3>
+        <span id="circle">{value}%</span>
+        <progress id="determinate" value={value} min="0" max="100">95%</progress>
+        <br />
+        <span id="circle">{value2}%</span>
+        <progress id="determinate" value={value2} min="0" max="100">75%</progress>
+      </div>
+
+      <hr id='long' />
+
+      <h2 id="allP">All Products</h2>
+
+      <div className="grid">
+        {allProducts && allProducts.map((item) => (
+          <div className="card">
+            <img src={item.image} alt="#" />
+            <div className="container">
+              <p>Name: {item.name}</p>
+              <p>Net_price: {item.net_price}</p>
+              <p>Tax: {item.taxes}</p>
+              <p>Price: {item.price}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Products;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -23,7 +23,7 @@ const Signup = ({ setCanLogin, setIsAdminPage }) => {
             setCanLogin(true);
             navigate("/");
             setIsAdminPage(false);
-        }else {
+        } else {
             setCanLogin(false);
             setIPassword(true);
             setPassword('');
@@ -34,15 +34,15 @@ const Signup = ({ setCanLogin, setIsAdminPage }) => {
     useEffect(() => {
         const ses_username = sessionStorage.getItem('admin-username');
         const ses_password = sessionStorage.getItem('admin-password');
-        if (ses_password != null && ses_username != null){
+        if (ses_password != null && ses_username != null) {
             setRedirect(true);
             setTimeout(() => {
                 setCanLogin(true);
                 navigate("/");
                 setIsAdminPage(false);
                 setRedirect(false);
-            }, 1500);
-        }else{
+            }, 2000);
+        } else {
             setCanLogin(false);
             setIsAdminPage(true);
         }
@@ -51,50 +51,50 @@ const Signup = ({ setCanLogin, setIsAdminPage }) => {
     return (
         <div className='sign_up_main'>
             <div className="sign_up_container">
-                <img className='sign_up_img' src={adminlogo} alt="adminlogo"/>
+                <img className='sign_up_img' src={adminlogo} alt="adminlogo" />
                 <h2 className='sign_up_txt_top'>Admin Dashboard</h2>
                 <form className='sign_up_form' onSubmit={(e) => e.preventDefault()}>
-                    
+
                     <h4 id='sign_up_first_input'>Username</h4>
                     <input className='sign_up_input'
-                    type={'text'}
-                    placeholder={'Username (max: 10)'}
-                    required
-                    maxLength={10}
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)} />
+                        type={'text'}
+                        placeholder={'Username (max: 10)'}
+                        required
+                        maxLength={10}
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)} />
 
                     <h4>Email</h4>
                     <input className='sign_up_input'
-                    type={'email'}
-                    placeholder={'Email'}
-                    required
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)} />
+                        type={'email'}
+                        placeholder={'Email'}
+                        required
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)} />
 
                     <h4>Password</h4>
                     <input className='sign_up_input'
-                    type={'password'}
-                    placeholder={'Password'}
-                    required
-                    value={password}
-                    onChange={(e) => { setPassword(e.target.value); setIPassword(false); }} />
+                        type={'password'}
+                        placeholder={'Password'}
+                        required
+                        value={password}
+                        onChange={(e) => { setPassword(e.target.value); setIPassword(false); }} />
 
                     <h4>Confirm Password</h4>
                     <input className='sign_up_input'
-                    type={'password'}
-                    placeholder={'Confirm Password'}
-                    required
-                    value={cPassword}
-                    onChange={(e) => { setCPassword(e.target.value); setIPassword(false); }} />
+                        type={'password'}
+                        placeholder={'Confirm Password'}
+                        required
+                        value={cPassword}
+                        onChange={(e) => { setCPassword(e.target.value); setIPassword(false); }} />
 
                     <button className='sign_up_button' onClick={checksignup}>Sign Up</button>
 
-                    { iPassword && <h3 style={{ color: 'red', maxHeight: "30px" }}>Passwords do not match!!!</h3> }
+                    {iPassword && <h3 style={{ color: 'red', maxHeight: "30px" }}>Passwords do not match!!!</h3>}
 
-                    { redirect && <h3 style={{ color: 'red', maxHeight: "22px", marginTop: "5px" }}>Account found, Redirecting...</h3> }
+                    {redirect && <h3 style={{ color: 'red', maxHeight: "22px", marginTop: "5px", fontSize: "17px" }}>Account found, Redirecting...</h3>}
 
-                    <h3 className='sign_up_text'>already have an account, <span style={{ color: "rgb(100, 100, 255)", cursor: "pointer"}} onClick={() => navigate('/login')}>login</span></h3>
+                    <h3 className='sign_up_text'>already have an account, <span style={{ color: "rgb(100, 100, 255)", cursor: "pointer" }} onClick={() => navigate('/login')}>login</span></h3>
                 </form>
             </div>
         </div>
